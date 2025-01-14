d14a60b84ce9917d00aa067a936b04a0a587fd47
Stephen Asembo <165886904+Stephenasembo@users.noreply.github.com>
2025-01-14 21:57:15 +0300

    Create Append Feature
    The value gets appended at the very end of the list in
    linked lists

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
 class LinkedList {
   constructor() {
-    return {};
+    this.head = null;;
+  }
+  append(value, node = this.head) {
+    const appendedNode = new Node(value, null);
+    let lastNode = this.traversal(node);
+    if (!lastNode) {
+      this.head = appendedNode;
+    } else {
+      lastNode.nextNode = appendedNode;
+    }
+  }
+  traversal(node) {
+    if (node == null || node.nextNode === null) {
+      return node;
+    }
+    return this.traversal(node.nextNode);
   }
 }
 
 class Node {
-  constructor() {
-    this.value = null;
-    this.nextNode = null;
+  constructor(value = null, nextNode = null) {
+    this.value = value;
+    this.nextNode = nextNode;
   }
 }
+
+const list = new LinkedList();
+list.append('dog');
+list.append('hamster');
+list.append('fish');
+list.append('cow');
+const test = JSON.stringify(list);
+console.log(test);
